9e1f0741371e700032ddd1ea2d73bc817484a581
Nick Pisani <napisani@yahoo.com>
2022-09-28 09:41:36 -0400

    1.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 setup(
     name='procmux',
-    version='1.0.2',
+    version='1.0.3',
     author='Nick Pisani',
     author_email='napisani@yahoo.com',
     license='MIT',
