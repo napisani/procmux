8e48bc6f15c52b23f0737a29cdb0db2469655120
Nick Pisani <napisani@yahoo.com>
2022-10-01 15:56:01 -0400

    working on auto starting pids bug

--- app/log.py
+++ app/log.py
@@ -3,10 +3,3 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-
-
-# def _handle_exception(exc_type, exc_value, exc_traceback):
-#     logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
-#
-#
-# sys.excepthook = _handle_exception
